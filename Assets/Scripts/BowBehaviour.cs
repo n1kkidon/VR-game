69fb749e10f302237e56dd19d48230e8941c3f8e
n1kkidon <68920983+n1kkidon@users.noreply.github.com>
2023-10-15 19:56:08 +0300

    can somewhat pull bows string

--- Assets/Scripts/BowBehaviour.cs
+++ Assets/Scripts/BowBehaviour.cs
@@ -1,19 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class BowBehaviour : MonoBehaviour
+public class BowBehaviour : XRBaseInteractable
 {
-    // Start is called before the first frame update
-    void Start()
+    public static event Action<float> PullActionReleased;
+
+    public Transform start, end;
+    public GameObject notch;
+    public float pullAmount {get; private set;} = 0.0f;
+    private LineRenderer lineRenderer;
+    
+    private IXRSelectInteractor pullingInteractor = null;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+    public void SetPullInteractor(SelectEnterEventArgs args)
+    {
+        pullingInteractor = args.interactorObject;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Release()
     {
-        
+        PullActionReleased?.Invoke(pullAmount);
+        pullingInteractor = null;
+        pullAmount = 0f;
+        notch.transform.localPosition = new Vector3(notch.transform.localPosition.x, notch.transform.localPosition.y, 0f);
+        UpdateString();
     }
 
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        base.ProcessInteractable(updatePhase);
+        if(updatePhase == XRInteractionUpdateOrder.UpdatePhase.Dynamic)
+        {
+            if(isSelected)
+            {
+                Vector3 pullPosition = pullingInteractor.transform.position;
+                pullAmount = CalculatePull(pullPosition);
+                UpdateString();
+            }
+        }
+    }
+
+    private float CalculatePull(Vector3 pullPosition)
+    {
+        Vector3 pullDirection = pullPosition - start.position;
+        Vector3 targetDirection = end.position - start.position;
+        float maxLength = targetDirection.magnitude;
+
+        targetDirection.Normalize();
+        float pullValue = Vector3.Dot(pullDirection, targetDirection) / maxLength;
+        return Mathf.Clamp(pullValue, 0, 1);
+    }
+
+    private void UpdateString()
+    {
+        Vector3 linePosition = Vector3.forward * Mathf.Lerp(start.transform.localPosition.z, end.transform.localPosition.z, pullAmount );
+        notch.transform.localPosition = new Vector3(notch.transform.localPosition.x, notch.transform.localPosition.y, linePosition.z );
+        lineRenderer.SetPosition(1, linePosition);
+    }
 }
