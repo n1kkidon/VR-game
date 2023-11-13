2e99455b884cd7f07f0b493f779b01b9892d7960
Vygintas Lukošiūnas <113847988+vygluk@users.noreply.github.com>
2023-11-13 18:51:40 +0200

    Add game start text
    .

---
+++ Assets/GameStartTextScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameStartTextScript : MonoBehaviour
+{
+    public float displayTime = 5f; // Time in seconds the text will be displayed
+    public TextMeshProUGUI gameStartText; // Make the Text variable public
+
+    private void Start()
+    {
+        // Check if the Text component was found
+        if (gameStartText != null)
+        {
+            // Set the alpha to fully opaque
+            gameStartText.alpha = 1f;
+
+            // Start the coroutine to fade out the text after the specified display time
+            StartCoroutine(FadeOutText());
+        }
+        else
+        {
+            Debug.LogError("Text component not found. Make sure it exists as a child of the GameStart GameObject.");
+        }
+    }
+
+    IEnumerator FadeOutText()
+    {
+        // Wait for the specified display time
+        yield return new WaitForSeconds(displayTime);
+
+        // Gradually decrease the alpha over time
+        while (gameStartText.alpha > 0)
+        {
+            gameStartText.alpha -= Time.deltaTime / 2f; // Adjust the divisor to control the fade speed
+            yield return null;
+        }
+
+        // Ensure the alpha is fully transparent
+        gameStartText.alpha = 0;
+
+        // Disable the Text component
+        gameStartText.enabled = false;
+    }
+}
